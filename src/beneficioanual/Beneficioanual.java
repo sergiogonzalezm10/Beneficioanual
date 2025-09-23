71e6d18b1b2da5e59cb8ce1985afbfaf3fa864fd
sergi <sergi@DESKTOP-I7FRT4I>
2025-09-23 21:26:25 +0200

    Beneficioanual

---
+++ src/beneficioanual/Beneficioanual.java
@@ -0,0 +1,28 @@
+package beneficioanual;
+
+import java.util.Scanner;
+
+public class Beneficioanual {
+
+	public static void main(String[] args) {
+		double peras;
+		double manzanas;
+		double precioperas;
+		double preciomanzanas;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Kilos de peras al año: ");
+		peras = sc.nextDouble();
+		System.out.println("Kilos de manzanas al año: ");
+		manzanas = sc.nextDouble();
+		
+		precioperas = peras * 1.95;
+		preciomanzanas = manzanas * 2.35;
+		
+		System.out.println("El precio de las peras es de: " + precioperas + " €" );
+		System.out.println("El precio de las manzanas es de: " + preciomanzanas + " €" );
+		
+		sc.close();
+	}
+
+}
